ff4e55e999bfaf707c789e0af674cbccb752c627
RounakSethia <rounak21sethia@gmail.com>
2021-03-24 14:59:25 +0530

    Used Tuple instead of foreach

--- src/main/java/Analysis.java
+++ src/main/java/Analysis.java
@@ -9,7 +9,7 @@ public class Analysis {
         List<String> listOrphan = new ArrayList<>();
 
         for (Tuple2<String, Set<String>> user : userPair.collect()){
-            JavaPairRDD<String, Set<String>> goodRoles = rolePair.filter(line -> user._2.containsAll(line._2)).repartition(6);
+            JavaPairRDD<String, Set<String>> goodRoles = rolePair.filter(line -> user._2.containsAll(line._2)).repartition(3);
             goodRoles.cache();
             System.out.println(user);
             listRole.add(user._1 + removeRedundant(goodRoles));
